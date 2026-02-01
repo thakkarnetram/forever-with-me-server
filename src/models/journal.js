85f147ccd9b0294066068633b5b7380665789d5b
thakkarnetram <thakkarnetram10@gmail.com>
2026-02-01 22:13:56 +0530

    added journal controller related operations , added timestamps in journal and memory model

--- src/models/journal.js
+++ src/models/journal.js
@@ -4,18 +4,11 @@ const journalSchema = new mongoose.Schema({
     text:{
         type:String
     },
-    createdAt:{
-        type:Date,
-        default:Date.now()
-    },
     createdBy:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User"
     },
-    journalId:{
-        type:String
-    }
-})
+},  { timestamps: true })
 
 const journal = mongoose.model("Journal",journalSchema);
 module.exports = journal;
